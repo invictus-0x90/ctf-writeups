aff36da644fd07184bb9aae5402e32c004928d6b
Tim <Tim@WIN-NIJQ3MDJ9II>
2016-10-05 17:05:09 +0100

    final code, solves challenge

--- flare-on-2016/challenge4/challenge4/challenge4.cpp
+++ flare-on-2016/challenge4/challenge4/challenge4.cpp
@@ -35,22 +35,23 @@ int main()
 				next = fn();
 			}
 		}
-		
+
 		/* The order is incredibly important, need to figure this out properly */
 		/* Possible order, gained from the return value from each function stored in eax */
-		int order[] = {35,22,25,9,45,23,36,2,12,37,14,18,8,46,30,7,4,42,34,47,11,32,27,39,10,3,21,13,16,33,26,17,44,6,20,48,19,29,38,24,15,41,31,28,43,5,40,1};
-		
+		int order[] = { 35,22,25,9,45,23,36,2,12,37,14,18,8,46,30,7,4,42,34,47,11,32,27,39,10,3,21,13,16,33,26,17,44,6,20,48,19,29,38,24,15,41,31,28,43,5,40,1 };
+
 		/* Get address of function 50 (beep function) */
 		name = MAKEINTRESOURCEA(50);
 		fn = GetProcAddress(dll, name);
 		func = (beep)fn;
-
+		int freq[] = {0x1b8, 0x1b8, 0x1b8, 0x15d, 0x20b, 0x1b8, 0x15d, 0x20b, 0x1b8, 0x293, 0x293, 0x293, 0x2ba, 0x20b, 0x19f, 0x15d, 0x20b, 0x1b8};
+		int dur[] = { 500, 500, 500, 350, 150, 500, 350, 150, 1000, 500, 500, 500, 350, 150, 500, 350, 150, 1000 };
 		//int freq = 300, dur = 59;
 		for (int i = 0; i < 18; i++)
 		{
-			func(k1, k2);
+			func(freq[i], dur[i]);
 		}
 	}
-    return 0;
+	return 0;
 }
 
